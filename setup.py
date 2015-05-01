b652ee7b1ce67a4728ace1a94485f365ee0994b5
Lion Jin <gameman100@qq.com>
2015-05-01 20:51:29 +0800

    upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'datapacker',
+    version = '1.0.1',
+    py_modules = ['datapacker'],
+    author = 'Jin',
+    author_email = 'gameman100@qq.com',
+    url = '',
+    description = 'Game develop utils',
+    )
---
+++ smartcopy.py
@@ -0,0 +1,90 @@
+import sys, shutil, io, re, os
+
+class SmartCopy:
+
+    def __init( self ):
+        print("create SmartCopy instance")
+
+    # pure virtaul function, you may want to override it for the special case
+    # check if the file is with special case
+    def specialcase(self, file):
+        return False
+
+    # copy data: copy data from the input path to the output path with filter pattern
+    # inputpath: input path, not ends with '/'
+    # outputpath: output path, not ends with '/'
+    # patterns: string(with lower case) list, format filter (the files with filter format will be not included)
+    def copydata ( self, inputpath, outputpath, patterns ):
+
+        # check if output path exist
+        if not os.path.exists ( outputpath ):
+            print("out put path is not exist!".format(outputpath))
+            return
+
+        # change current dir to input dir
+        os.chdir(inputpath)
+
+        # walk all path
+        dirs = os.walk(inputpath)
+        
+        for top in dirs:
+            print("copy...{0}".format(top[0]))
+            for file in top[2]:
+                path = top[0]
+                
+                inputfile = os.path.join( path, file )
+                subpath = inputfile.replace( inputpath, "" )
+                
+                #outputfile = os.path.join(outputpath, subpath)
+                outputfile = "{0}{1}".format( outputpath, subpath )
+
+                # check if output dir exist
+                outputsubpath = outputfile.rpartition('\\')
+                outputsubpath = outputsubpath[0]
+                if not os.path.exists( outputsubpath ):
+                  os.mkdir ( outputsubpath )
+                  print( "create new dir: {0}".format( outputsubpath ) )
+
+                # check pattern
+                ispass = False
+                for pa in patterns:
+                    lowerstr = pa.lower()
+                    filename = file.lower()
+                    if filename.endswith(lowerstr):
+                        ispass = True
+                        break
+
+                if ispass:
+                    continue
+
+                # check special case
+                if self.specialcase(outputfile):
+                    continue
+                
+                # ok, we can copy the file now
+                shutil.copy (inputfile, outputfile)
+
+# example class
+class MLAniamtionCopy(SmartCopy):
+    def __init(self, pattern ):
+        print("create MLAniamtionCopy instance")
+        
+    # override
+    def specialcase(self, file):
+        filename = file.lower()
+        if filename.endswith(".max"):
+            nodot = file.rpartition(".")
+            if len(nodot[0])>0:
+                result = re.search("[0-9][0-9]$", nodot[0])
+
+                if result:
+                    return True
+        return False
+
+
+if __name__=='__main__':
+    # donot copy psd files
+    patterns = {".psd"}
+    smart = MLAniamtionCopy()
+    smart.copydata("D:/Card2/trunk/Art/rawdata/动画MAX文件", "D:/output", patterns )
+
